40ff1ba3dea657a3b85e3be054b7171040f5d34c
Felix Salim <ravenkelder@gmail.com>
2021-03-01 19:01:19 +1100

    Fix deployment

--- server/serve.mjs
+++ server/serve.mjs
@@ -1,6 +1,6 @@
 import handler from "serve-handler";
 import { createServer } from "http";
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 const server = createServer((request, response) => {
   return handler(request, response, {
@@ -9,5 +9,5 @@ const server = createServer((request, response) => {
 });
 
 server.listen(port, () => {
-  console.log(`Running at http://localhost:${port}`);
+  console.log(`Running at port ${port}`);
 });
